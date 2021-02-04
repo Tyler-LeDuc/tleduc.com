9a5dac300031cf0ccb917d3bf468de41da509305
Tyler LeDuc <tyler.a.leduc@gmail.com>
2021-02-03 20:31:26 -0700

    Updates

---
+++ precache-manifest.7b82aa1c9ee80ca5f8dfb3afc58e6b1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a751997f9cf54e8f7262ea0b150cf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cedc773bff2f427ab51",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "49db44e468a3b2571323",
+    "url": "/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "4cedc773bff2f427ab51",
+    "url": "/static/js/2.66408c91.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.66408c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49db44e468a3b2571323",
+    "url": "/static/js/main.9bd43a78.chunk.js"
+  },
+  {
+    "revision": "a37daf6858792dead04f",
+    "url": "/static/js/runtime-main.0a4263c0.js"
+  }
+]);
